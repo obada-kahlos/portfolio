c856064531ad8c89a120f0a769b359f938effbee
obada <obada.kahlous@nextinuous.com>
2022-11-03 14:44:22 +0300

    cv

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Input from '../../components/shared/input/Input'
 import Button from '../../components/shared/button/Button'
-import { Formik , Form , ErrorMessage , useField} from 'formik'
-import {initialValues , schema ,handleSendEmail} from './index'
+import { Formik, Form, ErrorMessage, useField } from 'formik'
+import { initialValues, schema, handleSendEmail } from './index'
 import { InputForm } from './model.js'
 
 import TelegramIcon from '@mui/icons-material/Telegram';
@@ -12,7 +12,8 @@ import Container from '../../components/shared/Containers/Containers'
 
 
 
-import backgroundImage from '../../assets/intro-bg.jpg'
+import contactImage from '../../assets/contact.svg'
+import LazyLoad from 'react-lazyload'
 
 const Contact = () => {
   return (
@@ -23,63 +24,59 @@ const Contact = () => {
           <p className='text-slate-200 sm:text-[24px] text-[20px] md:text-center text-left uppercase tracking-[1.5px]'> Get in Touch<span className='text-main-color'>!</span> </p>
           <h1 className='mb-0 md:text-center text-main-color text-left sm:text-[36px] text-[24px] font-bold uppercase tracking-[1.5px]'> Contact Me<span className='text-[#fff]'>.</span></h1>
         </div>
-
-        <div className=''>
-          <div className=''>
-            <Formik
+        <div className='grid grid-cols-12 gap-4 justify-center items-center'>
+          <div className='md:col-span-6 col-span-12'>
+            <div className=''>
+              <Formik
                 initialValues={initialValues}
                 validationSchema={schema}
-                onSubmit={(values , { resetForm }) => {
+                onSubmit={(values, { resetForm }) => {
                   handleSendEmail(values)
                   resetForm()
                 }}
-            >
+              >
                 <Form>
-                      <div className=''>
-                          {
-                              InputForm.map((item,key)=>(
-                                  <div className='my-[20px]' key={key}>
-                                      <div className='w-full relative'>
-                                          <Input 
-                                            as={item.as}
-                                            id={item.id}
-                                            name={item.name}
-                                            type={item?.type}
-                                            placeholder={item.placeholder}
-                                            cols={item.cols}
-                                            rows={item.rows}
-                                            autocomplete={item.autocomplete}
-                                            className={item.class}
-                                          />
-                                          <span className={item.iconStyle}> {item.icon} </span>
-                                      </div>
-                                      <ErrorMessage
-                                          name={item.name}
-                                          render={(msg) => <p className="error">{msg}</p>}
-                                      />
-                                  </div>
-                              ))
-                          }
-                      </div>
-                      <Button color='text-[#fff]'> Send Message <span className='send'> <TelegramIcon /> </span> </Button>
+                  <div className=''>
+                    {
+                      InputForm.map((item, key) => (
+                        <div className='my-[20px]' key={key}>
+                          <div className='w-full relative'>
+                            <Input
+                              as={item.as}
+                              id={item.id}
+                              name={item.name}
+                              type={item?.type}
+                              placeholder={item.placeholder}
+                              cols={item.cols}
+                              rows={item.rows}
+                              autocomplete={item.autocomplete}
+                              className={item.class}
+                            />
+                            <span className={item.iconStyle}> {item.icon} </span>
+                          </div>
+                          <ErrorMessage
+                            name={item.name}
+                            render={(msg) => <p className="error">{msg}</p>}
+                          />
+                        </div>
+                      ))
+                    }
+                  </div>
+                  <Button color='text-[#fff]'> Send Message <span className='send'> <TelegramIcon /> </span> </Button>
                 </Form>
-            </Formik>
-          </div>
+              </Formik>
+            </div>
 
+          </div>
+          <div className='md:col-span-6 col-span-12 md:mt-[0px] mt-[30px]'>
+            <LazyLoad>
+              <img src={contactImage} alt="" width='100%' />
+            </LazyLoad>
+          </div>
         </div>
 
 
       </Container>
-      {/* <Layout /> */}
-      {/* <style>
-        {`
-          body{
-            background: #151515 url(${backgroundImage}) no-repeat center bottom;
-            background-size: cover;
-            background-attachment: fixed;
-          }
-        `}
-      </style> */}
     </>
   )
 }
