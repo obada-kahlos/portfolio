e2c36445d29e293ef0a75d23ce7d947f929739da
obada <obada.kahlous@nextinuous.com>
2022-11-04 21:25:08 +0300

    btn to dounlad my CV

--- src/components/cv/cv.jsx
+++ src/components/cv/cv.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+// import { saveAs } from "file-saver";
 import './cv.css'
 const Cv = () => {
+    // const saveFile = () => {
+    //     saveAs(
+    //       "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+    //       "example.pdf"
+    //     );
+    //  };
     return (
         <div className="My-Cv">
             <div className="btn-style">
@@ -14,7 +21,7 @@ const Cv = () => {
                             <p className="cv-text"> Portfolio </p>
                         </div>
                     </Link>
-                    <a href="../../assets/obada's Resume (3).pdf" className="cv" download>
+                    <a href="cv.pdf" className="cv" download>
                         <div className="btn">
                             <span className='text-[22px]'><ion-icon name="play-outline"></ion-icon></span>
                             <p className="cv-text"> CV </p>
