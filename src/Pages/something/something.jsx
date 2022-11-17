01a9cd2e5b4aa1ba287d509ed397acf087747c71
obada <obada.kahlous@nextinuous.com>
2022-11-17 17:39:23 +0300

    update change

--- src/Pages/something/something.jsx
+++ src/Pages/something/something.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Something = () => {
   return (
     <div className='flex justify-center items-center w-full h-full'>
-        <h1 className='text-[#fff] md:text-[50px] text-[18px]'> Something </h1>
+        <h1 className='text-[#fff] md:text-[50px] text-[18px] font-serif'> No Thing... </h1>
     </div>
   )
 }
